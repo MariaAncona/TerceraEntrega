e62a247288a402186648bebe0bb0ab828757b00e
MariaAncona <maria.ancona18@gmail.com>
2024-08-26 14:53:41 -0600

    actualización

--- index.js
+++ index.js
@@ -105,30 +105,3 @@ alert ("Las calorías que requiere tu paciente son = " + harrisBenedict);
 console.log ("Las calorías que requiere tu paciente son = " + harrisBenedict);
 
 
-let menu = prompt("¿Quieres ideas de recetas para tu paciente?" + "\n Sí o No");
-if (menu === "no"){
-    alert ("Refresca tu navegador para comenzar de nuevo"); 
-} else if (menu === "si"){
-    let selMenu =prompt("Ingresa el rango de calorías que vas a mandarle a tu paciente \n\n1: 1300 - 1500 \n2: 1500 - 2000 \n3: 2000 - 2500");
-    if (selMenu == 1) {
-        alert ("Menú 1 \n\nLunes - Miércoles - Viernes: \n\nDesayuno: Tostada integral con tomate y jamón serrano. \nComida: Revuelto de pollo con verduras, arroz y salsa de soja. \nCena: Salmón a la plancha con espárragos \n\nMartes - Jueves: \n\nDesayuno: Yogurt griego bajo en grasa con arándanos y puñado de nueces. \nComida: Ensalada de garbanzos con tomate, pimientos de colores y atún en lata.\nCena: Pollo al curry con migas de coliflor. \n\nSábado y Domingo: \n\nDesayuno: Tostada integral con aceite, tomate y jamón york +85% \nComida: Hamburguesa con alto porcentaje de carne y ensalada de tomates.\nCena: Crema de verduras y tortilla francesa.");
-        console.log ("Menú entregado");
-        alert ("Refresca tu navegador para comenzar de nuevo");
-        console.log ("Terminó la consulta");
-    }else if (selMenu == 2) {
-        alert ("Menú 2 \n\nLunes - Miércoles - Viernes: \n\nDesayuno: Yogurt de proteínas con plátano troceado y arándanos. \nComida: Ternera magra salteada con verduras y patatas al horno. \nCena: Salmorejo con huevo duro picado. \n\nMartes - Jueves: \n\nDesayuno: Huevos con tocino, taza de café o té y dos frutas \nComida: Salmón asado con ensalada caprese y arroz \nCena: Sopa de tomate con queso de cabra y un pan tostado integral \n\nSábado y Domingo: \n\nDesayuno: Dos huevos fritos con un pan tostado, taza de café y 2 frutas \nComida: Ensalada de pollo con una cucharada grande hummus, una taza de pasta. \nCena: Sopa de cebolla.");
-        console.log ("Menú entregado");
-        alert ("Refresca tu navegador para comenzar de nuevo");
-        console.log ("Terminó la consulta");
-    }else if (selMenu ==3) {
-        alert ("Menú 2 \n\nLunes - Miércoles - Viernes: \n\nDesayuno: Dos huevos cocidos con verduras, dos panes tostado dos frutas a elegir \nComida: Hamburguesa con queso y tocino y vegetales asados. \nCena: Fajitas de carne con pimientos. \n\nMartes - Jueves: \n\nDesayuno: 4 Pancakes con plátano y miel de maple.  \nComida: Pasta a la bolognese y verduras. \nCena: Pollo con verduras \n\nSábado y Domingo: \n\nDesayuno: Omelett con espinacas y queso. taza de café y dos frutas \nComida: 2 rebanas de pizza con ensalda. \nCena: Un aguacate relleno de verduras con queso.");
-        console.log ("Menú entregado");
-        alert ("Refresca tu navegador para comenzar de nuevo"); 
-        console.log ("Terminó la consulta");
-    }else {
-        console.log ("Terminó la consulta");
-        alert ("Refresca tu navegador para comenzar de nuevo"); 
-    }
-} else{
-    alert ("Refresca tu navegador para comenzar de nuevo");
-}

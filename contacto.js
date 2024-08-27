98d10c671b17c8d8d4b570e1f0daf843f8b62d35
MariaAncona <maria.ancona18@gmail.com>
2024-08-27 14:09:45 -0600

    contacto

--- contacto.js
+++ contacto.js
@@ -1,16 +1,20 @@
-let formulario = document.querySelector('#formulario');
-let nombre = document.getElementById('text');
-let mensaje = document.getElementById('message');
-let email = document.getElementById('email');
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault()
 
-formulario.addEventListener ("submit", (e) => {
-    let form = e.target;
+    const nombre = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+    const telefono = document.getElementById('telefono').value;
 
-    const nombreStorage = form.children[0].value;
-    const emailStorage = form.children[1].value;
-    const  mensajeStorage = form.children[2].value;
 
-    localStorage.setItem("nombre", nombreStorage);
-    localStorage.setItem("email", emailStorage);
-    localStorage.setItem("mensaje", mensajeStorage );
-})
+    localStorage.setItem('nombre', nombre);
+    localStorage.setItem('email', email);
+    localStorage.setItem('telefono', telefono);
+
+    document.getElementById('enviado').style.display = 'block';
+
+    document.getElementById('contactForm').reset();
+        });
+
+    
+
+
